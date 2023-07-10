be4b009875c3aa17a80dcc1cfd655ed3a4d9b7cb
sygem <simon@sygem.com>
2023-07-10 18:48:12 +0100

    I hate dependencies

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\SyGem\flutter_base"
+export "FLUTTER_APPLICATION_PATH=F:\Flux\flutter_base"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.9"
+export "FLUTTER_BUILD_NUMBER=0.0.9"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
