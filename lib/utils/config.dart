a5f1f9dd5c88e10779716118c5d80cd0dac4abcb
sygem <simon@sygem.com>
2023-09-04 15:19:32 +0100

    App config constants

---
+++ lib/utils/config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart' show Size;
+
+const Size kDesignSize = Size(1920, 1080);
+
+const kSmallDeviceScreenWidth = 1400;
+const kSmallDeviceScreenHeight = 800.0;
+const kSmallScreenWidth = 800.0;
+const kMinWidth = 500.0;
+const kMaxWidth = 1920.0;
+
+/// how often to refresh the monitoring data
+const kHardwareMonitoringRefresh = 2000;
+
+const kMinerMonitoringRefresh = 4000;
+
+/// ms
+
+const kAutoCloseDrawerOnNavigation = false;
