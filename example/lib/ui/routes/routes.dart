776779e24d41de087dc85e3a9a4500c46e926e27
sygem <simon@sygem.com>
2023-09-07 09:43:12 +0100

    Example route names and icons

--- example/lib/ui/routes/routes.dart
+++ example/lib/ui/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base/ui/routes/route.dart';
 import 'package:flutter_base/ui/routes/routes.dart';
+import 'package:flutter_base/ui/widgets/popup_message.dart';
 import 'package:flutter_base_example/ui/screens/home/home_screen.dart';
 
 class ExampleAppRouter extends AppRouter {
@@ -24,41 +25,42 @@ class ExampleAppRouter extends AppRouter {
         route: '/disabled',
         body: HomeScreen(),
         title: 'Disabled',
-        icon: Icons.home,
+        icon: Icons.disabled_by_default_outlined,
         includeInMenu: true,
         active: false,
       ),
       RouteSet(
-        title: 'Workers',
-        asset: 'assets/images/svg/workers_icon.svg',
+        title: 'Sub Menu',
+        asset: 'assets/images/svg/mining_icon.svg',
         routes: [
           NavigationRoute(
             route: '/1',
             body: HomeScreen(),
             title: '1',
-            icon: Icons.home,
+            icon: Icons.access_alarm_outlined,
             includeInMenu: true,
           ),
           NavigationRoute(
             route: '/2',
             body: HomeScreen(),
             title: '2',
-            icon: Icons.home,
+            icon: Icons.back_hand_outlined,
             includeInMenu: true,
           ),
           NavigationRoute(
             route: '/3',
             body: HomeScreen(),
             title: '3',
-            icon: Icons.home,
+            icon: Icons.three_g_mobiledata_outlined,
             includeInMenu: true,
           ),
         ],
       ),
       ActionRoute(
         title: 'Action',
-        action: () {
+        action: (BuildContext context) {
           debugPrint('do something!!');
+          const PopupMessage(message: 'Do Something!!').show(context);
         },
         route: '/action',
         icon: Icons.add,
