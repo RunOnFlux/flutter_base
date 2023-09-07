0249ce461dfab903ee6b7eb48c99f6aa3fe5c4f1
sygem <simon@sygem.com>
2023-09-07 10:48:43 +0100

    Debug logging

--- example/lib/ui/app/loading.dart
+++ example/lib/ui/app/loading.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_base/ui/app/i18n.dart';
 import 'package:flutter_base/ui/app/loading_notifier.dart';
 
 class ExampleLoadingNotifier extends LoadingNotifier {
   @override
   Future<void> fetchData() async {
+    debugPrint('LoadingNotifier started');
     await MyI18n.loadTranslations();
     loadingComplete = true;
+    debugPrint('LoadingNotifier notifying');
     notifyListeners();
   }
 }
