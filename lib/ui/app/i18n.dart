65471152d20eafd0b288c5f980dc38b05bfe0cbf
sygem <simon@sygem.com>
2023-11-08 17:07:11 +0000

    Remove i18n for now

--- lib/ui/app/i18n.dart
+++ lib/ui/app/i18n.dart
@@ -1,16 +1,15 @@
-import 'package:i18n_extension/i18n_extension.dart';
-import 'package:i18n_extension/io/import.dart';
+//import 'package:i18n_extension/io/import.dart';
 
 class MyI18n {
-  static TranslationsByLocale translations = Translations.byLocale("en");
+  //static TranslationsByLocale translations = Translations.byLocale("en");
 
   static Future<void> loadTranslations({String? assetLocation}) async {
-    translations += await GettextImporter().fromAssetDirectory(assetLocation ?? 'assets/locales');
+    //translations += await GettextImporter().fromAssetDirectory(assetLocation ?? 'assets/locales');
   }
 }
 
 extension Localization on String {
-  String get i18n => localize(this, MyI18n.translations);
-  String plural(value) => localizePlural(value, this, MyI18n.translations);
-  String fill(List<Object> params) => localizeFill(this, params);
+  //String get i18n => localize(this, MyI18n.translations);
+  //String plural(value) => localizePlural(value, this, MyI18n.translations);
+  //String fill(List<Object> params) => localizeFill(this, params);
 }
