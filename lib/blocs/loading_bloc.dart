75d2ad8bc40b1a6a65f6c4aeff6e2f0c4e39d1c4
sygem <simon@sygem.com>
2024-02-13 14:34:11 +0000

    Unseal the loading event class to allow subclassing

--- lib/blocs/loading_bloc.dart
+++ lib/blocs/loading_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-sealed class LoadingEvent {}
+class LoadingEvent {}
 
 final class StartLoadingApp extends LoadingEvent {}
 
