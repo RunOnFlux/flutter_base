48376d50275970e180d728b21f0f6370b0001464
sygem <simon@sygem.com>
2024-08-12 14:44:56 +0100

    More Flutter 3.24 colour fixes

--- lib/extensions/modal_sheet_extensions.dart
+++ lib/extensions/modal_sheet_extensions.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter_base/ui/theme/app_theme.dart';
 import 'package:tinycolor2/tinycolor2.dart';
 
 extension ModalSheet on BuildContext {
@@ -7,14 +7,23 @@ extension ModalSheet on BuildContext {
     WidgetBuilder builder, {
     bool useRootNavigator = true,
     bool enableDrag = true,
+    Color? backgroundColor,
+    bool isDismissible = true,
   }) {
-    showMaterialModalBottomSheet(
+    showModalBottomSheet(
       context: this,
-      backgroundColor: Theme.of(this).canvasColor.lighten().withOpacity(0.9),
+      backgroundColor: backgroundColor ??
+          (Theme.of(this).isLight
+                  ? Theme.of(this).primaryColorLight.lighten()
+                  : Theme.of(this).primaryColorDark.darken())
+              .withOpacity(0.9),
       enableDrag: enableDrag,
-      isDismissible: true,
+      isDismissible: isDismissible,
       useRootNavigator: useRootNavigator,
       builder: builder,
+      isScrollControlled: true,
+      constraints: const BoxConstraints(minWidth: 1200),
+      //useSafeArea: true,
     );
   }
 }
