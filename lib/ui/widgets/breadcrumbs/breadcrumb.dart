e70cc4bd2762953de61e4b4791cfca6bc27a3b91
sygem <simon@sygem.com>
2025-03-21 14:44:27 +0000

    Breadcrumb widget

---
+++ lib/ui/widgets/breadcrumbs/breadcrumb.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'breadcrumb_item.dart';
+import 'breadcrumb_overflow.dart';
+
+typedef IndexedBreadCrumbItemBuilder = BreadCrumbItem Function(int index);
+
+class BreadCrumb extends StatelessWidget {
+  final List<BreadCrumbItem> items;
+  final Widget? divider;
+  final BreadCrumbOverflow overflow;
+
+  const BreadCrumb({super.key, required this.items, this.divider, this.overflow = const WrapOverflow()})
+    : assert(items.length > 0, 'items parameters should not be empty');
+
+  factory BreadCrumb.builder({
+    required int itemCount,
+    required IndexedBreadCrumbItemBuilder builder,
+    Widget? divider,
+    BreadCrumbOverflow overflow = const WrapOverflow(),
+  }) => BreadCrumb(
+    items: List<BreadCrumbItem>.generate(itemCount, (i) => builder(i)),
+    divider: divider,
+    overflow: overflow,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return overflow.build(context, items, divider);
+  }
+}
---
+++ lib/ui/widgets/breadcrumbs/breadcrumb_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'breadcrumb_item.dart';
+
+class BreadCrumbTile extends StatelessWidget {
+  final BreadCrumbItem breadCrumbItem;
+
+  const BreadCrumbTile({super.key, required this.breadCrumbItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: breadCrumbItem.margin,
+      child: Material(
+        color: breadCrumbItem.isEnable ? breadCrumbItem.color : breadCrumbItem.disableColor,
+        shape: RoundedRectangleBorder(borderRadius: breadCrumbItem.borderRadius, side: breadCrumbItem.border),
+        child: InkWell(
+          onTap: breadCrumbItem.onTap,
+          splashColor: breadCrumbItem.splashColor,
+          borderRadius: breadCrumbItem.borderRadius as BorderRadius?,
+          child: Padding(
+            padding: breadCrumbItem.padding,
+            child: DefaultTextStyle.merge(
+              style: TextStyle(
+                color: breadCrumbItem.isEnable ? breadCrumbItem.textColor : breadCrumbItem.disabledTextColor,
+              ),
+              child: breadCrumbItem.content,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
